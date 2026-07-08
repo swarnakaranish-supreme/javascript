a575e606a913121b05386daa4e7726708ac18804
Anish Swarnakar <swarnakaranish@gmail.com>
2026-07-08 17:12:31 +0530

    updating some part of day 1

---
+++ day 1/script.js
@@ -0,0 +1,15 @@
+// i dont know why i am writting this its of no use beacause its a comment 
+console.log("day 1 of learning javascript");
+console.log("my name is anish swarnakar");
+console.log("i am learning aiml:");
+console.log("i want to become a software engineer");
+/* it is a multi line comment
+with multiple line */
+let name = "anish swarnakar";
+let age = 19;
+let isStudentofbrainwareuniversity = true;
+let city = "kharagpur";
+console.log(name);
+console.log(age);
+console.log(isStudentofbrainwareuniversity);
+console.log(city);
